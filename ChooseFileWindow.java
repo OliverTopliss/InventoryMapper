b635887bf262065df562dc32abb70ded00b13860
Oliver Topliss <oliver.topliss@student.manchester.ac.uk>
2019-07-09 00:33:45 +0100

    added comments and code to allow for the layering of images over the map when the map is clicked

--- ChooseFileWindow.java
+++ ChooseFileWindow.java
@@ -4,42 +4,63 @@ import java.awt.event.ActionListener;
 import javax.swing.JFrame;
 import javax.swing.JFileChooser;
 import javax.swing.filechooser.FileNameExtensionFilter;
+import java.io.File;
 
+//class that is used to model the GUI for choosing a map file to open
 public class ChooseFileWindow extends JFrame implements ActionListener
 {
+  //default chosen file is nothing
+  private String fileLocation = "";
+
+  //choosefilewindow constrcutor
   public ChooseFileWindow()
   {
     setTitle("Choose File");
     Container contents = getContentPane();
+    //deafult location to load the file chooser is in the current directory
     JFileChooser fileChooser = new JFileChooser("./");
     contents.add(fileChooser);
 
+    //filter is applied for png and jpeg images so only these files are displayed by default
     FileNameExtensionFilter fileFilter = new FileNameExtensionFilter("JPEG images and png images", "png", "JPEG");
     fileChooser.setFileFilter(fileFilter);
+
+    //filter to search for all file types is disabled
     fileChooser.setAcceptAllFileFilterUsed(false);
+
     //gets the button that was pressed on the file chooser: open or cancel
     int buttonPressed = fileChooser.showOpenDialog(this);
 
     //if the button is the open button then...
     if(buttonPressed == fileChooser.APPROVE_OPTION)
+    {
       System.out.println("yes");
+      File selectedFile = fileChooser.getSelectedFile();
+
+      fileLocation = fileChooser.getName(selectedFile);
+    }//if
     //otherwise if the cancel button is pressed then do...
     else if(buttonPressed == fileChooser.CANCEL_OPTION)
+    {
       System.out.println("cancel");
+    }//elseIf
+
     setVisible(true);
     pack();
-  }
+  }//ChooseFileWindow constructor
 
+  //currently does nothing
   @Override
   public void actionPerformed(ActionEvent event)
   {
 
 
-  }
+  }//actionPerformed
 
-  /*@Override
-  public void approveSelection()
+  //method used to get the location of the map file
+  public String getFileLocation()
   {
+    return fileLocation;
+  }// getFileLocation
 
-  }*/
-}
+}//ChooseFileWindow class
--- InventoryMapper.java
+++ InventoryMapper.java
@@ -3,49 +3,125 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JFrame;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.io.File;
 
-public class InventoryMapper extends JFrame implements ActionListener
+//program that is used for mapping the location of devices when completing an inventory
+public class InventoryMapper extends JFrame implements ActionListener, MouseListener
 {
-  private JPanel fileChooserPanel = new JPanel();
-  private JButton selectFileButton = new JButton("Select File");
 
+  private JPanel fileChooserPanel = new JPanel();
+  private JPanel mapPanePanel = new JPanel();
+  private JLayeredPane mapPane = new JLayeredPane();
+  private JButton selectFileButton = new JButton("Select File");
+  private ImageIcon mapImage = null;
+  private JLabel mapImageLabel = new JLabel();
+  private Container contents = getContentPane();
+
+  //constructor method
   public InventoryMapper()
   {
+
+    //gives the GUI a title, layout and adds components to it
     setTitle("Inventory Mapper");
-    Container contents = getContentPane();
+
     contents.setLayout(new BorderLayout());
 
+    //adds a panel in the ceter to hold the map
+    contents.add(mapPanePanel, BorderLayout.CENTER);
 
+    //the panel that holds the map has no layout to allow components to be placed at coordinates
+    mapPanePanel.setLayout(null);
+
+    //the button to open the map image file is stored in a flow layout
     fileChooserPanel.setLayout(new FlowLayout());
 
     fileChooserPanel.add(selectFileButton);
 
     contents.add(fileChooserPanel, BorderLayout.NORTH);
+    //the button has an action listener associated with it
     selectFileButton.addActionListener(this);
 
     pack();
   }// InventoryMapper Constructor
 
+
   public static void main (String[] args)
   {
     InventoryMapper inventoryMapper = new InventoryMapper();
     inventoryMapper.setVisible(true);
-  }
+  }// main
 
+  //performed when the button to open the file is clicked
   @Override
   public void actionPerformed(ActionEvent event)
   {
     if (event.getSource() == selectFileButton)
     {
+      //creates a new window for choosing a file to load
       ChooseFileWindow selectFileWindow = new ChooseFileWindow();
 
+      //the image of the map is stored as an image icon and created from the file selected
+      mapImage = new ImageIcon(selectFileWindow.getFileLocation());
+      //the image icon is added to the label
+      mapImageLabel.setIcon(mapImage);
+
+      //the bounds of the layered pane are set which must be done for the layered pane to work correctly
+      mapPane.setBounds(0,0, mapImage.getIconWidth(),mapImage.getIconHeight());
+      //the layered pane is added to the GUI in another panel
+      mapPanePanel.add(mapPane);
+      //the map image is added to the bottom most layer of the the layered panel
+      mapPane.add(mapImageLabel,JLayeredPane.DEFAULT_LAYER);
+
+      mapImageLabel.setBounds(0,0, mapImage.getIconWidth(), mapImage.getIconHeight());
+
+      //the image of the map has a mouse listener associated with it
+      mapImageLabel.addMouseListener(this);
+      pack();
+    }// if
+  }// actionPerformed
+
+  @Override
+  public void mouseClicked(MouseEvent event)
+  {
+    //when the mouse is clicked on the image of the map, a new image is created and added to a label
+    ImageIcon dotImage = new ImageIcon("./dot.png");
+
+    JLabel dot = new JLabel(dotImage);
+    dot.setBounds(event.getX(), event.getY(), 10, 10);
+
+    //the dot image is added to the GUI at the coordinates of the click
+    mapPane.add(dot, JLayeredPane.DEFAULT_LAYER + 1, 0);
+    pack();
+  }// mouseClicked
+
+  //methods don't currently do anything
+  @Override
+  public void mouseEntered(MouseEvent event)
+  {
+  }// mouseEntered
+
+  @Override
+  public void mouseExited(MouseEvent event)
+  {
+  }// mouseExited
+
+  @Override
+  public void mousePressed(MouseEvent event)
+  {
+  }// mousePressed
+
+  @Override
+  public void mouseReleased(MouseEvent event)
+  {
+  }// mouseReleased
 
 
-    }
-  }
-
+  //used to get a reference to the button to select a file
   public JButton getSelectFileButton()
   {
     return selectFileButton;
-  }
-}
+  }// getSeledtedFileButton
+
+}// InventoryMapper Class
