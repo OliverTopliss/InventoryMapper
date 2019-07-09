938bd3fb0c13ca5a05d0b01fb3b8f44f6c9623ed
Oliver Topliss <oliver.topliss@student.manchester.ac.uk>
2019-07-09 15:34:31 +0100

    created a class to model a point that a user places on the map

---
+++ MapPoint.java
@@ -0,0 +1,17 @@
+public class MapPoint
+{
+  private int xCoordinate = 0;
+  private int yCoordinate = 0;
+  private String name = "";
+  private String details = "";
+
+
+  public MapPoint(int xCoordinate, int yCoordinate, String name, String details)
+  {
+    this.xCoordinate = xCoordinate;
+    this.yCoordinate = yCoordinate;
+    this.name = name;
+    this.details = details;
+  }//MapPoint
+
+}//MapPoint class
