4fbaea8feb981120a5f2f88b93f106b984f64001
Oliver Topliss <oliver.topliss@student.manchester.ac.uk>
2019-07-11 15:26:02 +0100

    added a toString and compareTo method as well as accessor methods for the x and y coordinates

--- MapPoint.java
+++ MapPoint.java
@@ -1,4 +1,5 @@
-public class MapPoint
+//class that is used to model a point on the map
+public class MapPoint implements Comparable<MapPoint>
 {
   private int xCoordinate = 0;
   private int yCoordinate = 0;
@@ -14,4 +15,40 @@ public class MapPoint
     this.details = details;
   }//MapPoint
 
+  @Override
+  public int compareTo(MapPoint other)
+  {
+    if(other.getXCoordinate() < xCoordinate || other.getYCoordinate() < yCoordinate)
+    {
+      return -1;
+    }// if
+    else if(other.getXCoordinate() == xCoordinate || other.getYCoordinate() == other.yCoordinate)
+    {
+      return 0;
+    }//else if
+    else
+    {
+      return 1;
+    }//else
+  }//compareTo
+
+
+  //the string format that the object should posses (overridden from Object)
+  @Override
+  public String toString()
+  {
+    return xCoordinate + ", " + yCoordinate + ", " + name + ", " + details;
+  }//toString
+
+  //accessor method for the X Coordinate
+  public int getXCoordinate()
+  {
+    return xCoordinate;
+  }//getXCoordinate
+
+  //accessor method for the Y Coordinate
+  public int getYCoordinate()
+  {
+    return yCoordinate;
+  }//getYCoordinate
 }//MapPoint class
